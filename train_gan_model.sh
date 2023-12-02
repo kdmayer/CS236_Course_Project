0be81a5089757ef7e1fc714e1b32eaa0272b203b
Kevin <kdmayer92@gmail.com>
2023-12-02 14:00:47 -0800

    full training run

--- train_gan_model.sh
+++ train_gan_model.sh
@@ -2,6 +2,6 @@
 #SBATCH -p gpu
 #SBATCH -c 5
 #SBATCH --gres gpu:1
-#SBATCH --time=1:00:00
+#SBATCH --time=12:00:00
 
-python3 train_gan_model.py -l sinkhorn -l energy -l gaussian -l laplacian --input_dir mock_data
+python3 train_gan_model.py -l sinkhorn -l energy -l gaussian -l laplacian --input_dir data
